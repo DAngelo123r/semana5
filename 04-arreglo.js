181e9790a58877725103650ada1e4a24fa83df16
DAngelo123r <89820210+DAngelo123r@users.noreply.github.com>
2021-09-23 21:26:43 -0500

    update

---
+++ 04-arreglo.js
@@ -0,0 +1,8 @@
+let nombres= ["Masami","Oscar","Pedro","Maruja","Carlos"];
+
+console.log(nombres[1]);
+console.log(`los nombres de mis compañeros son ${nombres[0]},${nombres[1]},${nombres[2]}`);
+
+console.log(nombres);
+
+let encuentra= nombres.findIndex((item))
