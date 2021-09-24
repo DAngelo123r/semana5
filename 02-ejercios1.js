f3e9416e3dce9c55f7e607d156a9e694c8ef7b9f
DAngelo123r <89820210+DAngelo123r@users.noreply.github.com>
2021-09-23 19:58:33 -0500

    update

---
+++ 02-ejercios1.js
@@ -0,0 +1,21 @@
+//Pedir al usuario sus nombres, apellidos y edad cada uno por separado, el resultado deberia ser un console.log, resumiendo los datos del usuario.
+
+//let nombres = prompt("Cuál es tu nomre?");
+//let apellidos = prompt("Cuales son tus apellidos?");
+//let edad = prompt("Cuál es tu edad?");
+
+//console.log(`Bienvenid@: ${nombres}${apellidos} de ${edad} años de edad`);
+//a) Qué datos necesito?
+//rpta: la base y la altura
+//b) comó puedo tener esos datos??
+//prompt
+//c) Esos datos están en la forma q necesito?
+//mejor lo convertimos a number
+
+
+let base = +prompt("Cual es la base del triangulo?")
+let altura = +prompt("Cual es la altura del triangulo?")
+let resultado = base*altura/2;
+console.log(resultado);
+
+
