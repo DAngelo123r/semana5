5546c715a106b35eb0b46f0114ddffccfb29c511
DAngelo123r <89820210+DAngelo123r@users.noreply.github.com>
2021-09-27 21:03:45 -0500

    update

---
+++ reto-1/reto1.js
@@ -0,0 +1,20 @@
+//L cantidad de litros que produce ENTERO
+//PG precio del galón REAL
+//TG cantidad de galones que produce REAL
+//GA ganancia por la entrega de leche REAL
+
+
+//1 galon = 3.785 litros
+
+
+let precioGalone = +prompt("Cuánto es el precio del galón de leche?");
+
+let cantidadGalones = +prompt("Cuántos galones se vendieron");
+
+const galonexlitro = 3.785;
+
+let conversionAlitros = cantidadGalones * galonexlitro;
+
+let ganancia = precioGalone * conversionAlitros;
+
+alert(`Recibira ${ganancia} por la entrega de produccion de hoy.`);
